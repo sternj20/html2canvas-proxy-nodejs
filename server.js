8420944282df4acca7c1e53cdea779f2a852c394
sternj20 <jessesternmusic@gmail.com>
2017-11-22 09:57:15 -0800

    restructure

--- server.js
+++ server.js
@@ -1,48 +1,5 @@
-const express = require('express');
-const url = require('url');
-const cors = require('cors');
-const request = require('request');
-const port = (process.env.PORT || 3000);
-const app = express();
-const router = express.Router();
+var proxy = require('html2canvas-proxy');
+var express = require('express');
 
-
-function validUrl(req, res, next) {
-    if (!req.query.url) {
-        next(new Error('No url specified'));
-    } else if (typeof req.query.url !== 'string' || url.parse(req.query.url).host === null) {
-        next(new Error(`Invalid url specified: ${req.query.url}`));
-    } else {
-        next();
-    }
-}
-
-    router.get('/', cors(), validUrl, (req, res, next) => {
-        console.log(req.query)
-        switch (req.query.responseType) {
-    case 'blob':
-        req.pipe(request(req.query.url).on('error', next)).pipe(res);
-        break;
-    case 'text':
-    default:
-        request({url: req.query.url, encoding: 'binary'}, (error, response, body) => {
-            if (error) {
-                return next(error);
-            }
-            res.send(
-            `data:${response.headers['content-type']};base64,${Buffer.from(
-                body,
-                'binary'
-            ).toString('base64')}`
-        );
-    });
-    }
-});
-
-
-
-
-console.log("Server running on port", port);
-
-app.use('/', router)
-app.listen(port);
+var app = express();
+app.use('/', proxy());
