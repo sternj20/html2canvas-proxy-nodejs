0a6e0033a6596e3fa7656e08a2b3513398be0b13
sternj20 <jessesternmusic@gmail.com>
2017-11-22 11:15:59 -0800

    adding headers in server js file

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ var express = require('express');
 var app = express();
 app.use('/', proxy());
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 console.log("Server running on port", port);
 
