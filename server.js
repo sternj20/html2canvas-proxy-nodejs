a796c2d20123be8c625e0c75fddb3465aac5e31f
sternj20 <jessesternmusic@gmail.com>
2017-11-22 10:02:29 -0800

    running server (:

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+
+const port = (process.env.PORT || 3000);
 var proxy = require('html2canvas-proxy');
 var express = require('express');
 
 var app = express();
 app.use('/', proxy());
+
+
+
+console.log("Server running on port", port);
+
+app.listen(port);
