f8c0981c6bab4bf0f08d5a52bb5807f4a9edb660
sternj20 <jessesternmusic@gmail.com>
2017-11-24 08:59:34 -0800

    fixed routes

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ function validUrl(req, res, next) {
     }
 }
 
-const router = express.Router();
-router.get('/', cors(), validUrl, (req, res, next) => {
+// const router = express.Router();
+app.get('/', cors(), validUrl, (req, res, next) => {
         // console.log(req.query)
         let options = req.query.url
         // options += "&Signature=P4DN9uv5vfNHZ6oTzY2CfQdvzXcoNb5u8nEJRn8aVNV2FaFZ1wW3xbf48E7Y7jJ~tofOXRN9X0y2mrJMhaanaJ-c7rb6DwYO9yd~T6rGxrMkW7VSHmxKVeCdgFV-4lpVVY7-A-rxvXaQX~adJA9eo~mbYQui12FGmSGu5it6Q9wtVVJW6xkltWKgTus-3-8HLOSGWnH0s9KZYxz-LEJ74P4dfvyDPGuTMvAfy2hNZLufFlWAsXgrX4p0kjENaXq~~G0aUZvhLzZXbBqruBufBt4o4cWZRQFKAeez9rSfueqvFA8cd6M6iOi6XPfYpTQBwfuNO04qhJnflpsFiLwh6A__&Key-Pair-Id=APKAJXGC45PGQXCMCXSA"
