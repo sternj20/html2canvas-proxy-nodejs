9794369e3f42d25f6d259b2be402e1ae1b8c455f
sternj20 <jessesternmusic@gmail.com>
2017-11-26 15:12:34 -0800

    added url string and concat onto that the signature and keypair id for url templates

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 
-var port = (process.env.PORT || 3000);
+const port = (process.env.PORT || 3000);
 const express = require('express');
+const app = express();
 const url = require('url');
 const cors = require('cors');
-var request = require('request')
-var app = express();
+const request = require('request');
+
 function validUrl(req, res, next) {
     if (!req.query.url) {
         next(new Error('No url specified'));
@@ -15,33 +16,23 @@ function validUrl(req, res, next) {
     }
 }
 
-// const router = express.Router();
 app.get('/', cors(), validUrl, (req, res, next) => {
-        // console.log(req.query)
-        let options = req.query.url
-        // options += "&Signature=P4DN9uv5vfNHZ6oTzY2CfQdvzXcoNb5u8nEJRn8aVNV2FaFZ1wW3xbf48E7Y7jJ~tofOXRN9X0y2mrJMhaanaJ-c7rb6DwYO9yd~T6rGxrMkW7VSHmxKVeCdgFV-4lpVVY7-A-rxvXaQX~adJA9eo~mbYQui12FGmSGu5it6Q9wtVVJW6xkltWKgTus-3-8HLOSGWnH0s9KZYxz-LEJ74P4dfvyDPGuTMvAfy2hNZLufFlWAsXgrX4p0kjENaXq~~G0aUZvhLzZXbBqruBufBt4o4cWZRQFKAeez9rSfueqvFA8cd6M6iOi6XPfYpTQBwfuNO04qhJnflpsFiLwh6A__&Key-Pair-Id=APKAJXGC45PGQXCMCXSA"
-        // options = "https://public-tiles.dronedeploy.com/a11cf8293a_JESSESTERNOPENPIPELINE_ortho_moo/20/242484/412020.png?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9wdWJsaWMtdGlsZXMuZHJvbmVkZXBsb3kuY29tL2ExMWNmODI5M2FfSkVTU0VTVEVSTk9QRU5QSVBFTElORV9vcnRob19tb28vKiIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MjE0NTkxNjU1OX19fV19&Signature=P4DN9uv5vfNHZ6oTzY2CfQdvzXcoNb5u8nEJRn8aVNV2FaFZ1wW3xbf48E7Y7jJ~tofOXRN9X0y2mrJMhaanaJ-c7rb6DwYO9yd~T6rGxrMkW7VSHmxKVeCdgFV-4lpVVY7-A-rxvXaQX~adJA9eo~mbYQui12FGmSGu5it6Q9wtVVJW6xkltWKgTus-3-8HLOSGWnH0s9KZYxz-LEJ74P4dfvyDPGuTMvAfy2hNZLufFlWAsXgrX4p0kjENaXq~~G0aUZvhLzZXbBqruBufBt4o4cWZRQFKAeez9rSfueqvFA8cd6M6iOi6XPfYpTQBwfuNO04qhJnflpsFiLwh6A__&Key-Pair-Id=APKAJXGC45PGQXCMCXSA"
-        // options = "https://s7d1.scene7.com/is/image/PETCO/dog-category-090617-369w-269h-hero-cutout-d?fmt=png-alpha"
-        console.log (options)
-        
-        switch (req.query.responseType) {
-            case 'blob':
-            console.log('blob')
 
-            req.pipe(request(req.query.url).on('error', next)).pipe(res);
-            break;
-            case 'text':
-            console.log('text')
-            default:
-            console.log('default')
-
-
-            request(options, (error, response, body) => {
-                if (error) {
-                    return next(error);
-                }
-            // console.log(response)
-            console.log(body)
+    switch (req.query.responseType) {
+        case 'blob':
+        req.pipe(request(req.query.url).on('error', next)).pipe(res);
+        break;
+        case 'text':
+        default:
+        let url = ''
+        url += req.query.url
+        url += `&Signature=${req.query.Signature}`
+        url += `&Key-Pair-Id=${req.query["Key-Pair-Id"]}`
+        console.log(url)
+        request({url: url, encoding: 'binary'}, (error, response, body) => {
+            if (error) {
+                return next(error);
+            }
             res.send(
                 `data:${response.headers['content-type']};base64,${Buffer.from(
                     body,
@@ -49,10 +40,8 @@ app.get('/', cors(), validUrl, (req, res, next) => {
                     ).toString('base64')}`
                 );
         });
-        }
-    });
-
-
+    }
+});
 
 
 console.log("Server running on port", port);
